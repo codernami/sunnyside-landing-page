7b46980db29f05e4154778b159893e56d8a74f7f
codernami <codernami@gmail.com>
2023-02-27 15:00:11 -0300

    Adding files

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const btn_hamburguer = document.getElementById('btn-hamburguer');
+const svg_hamburguer = document.querySelector('.svg-hamburguer');
+const nav_Menu = document.querySelector('.navMenu');
+const shape = document.querySelector('.triangle');
+
+btn_hamburguer.addEventListener('click', () => {
+    
+    svg_hamburguer.classList.toggle('is-active');
+    nav_Menu.classList.toggle('is-active');
+    shape.classList.toggle('is-active');
+});
+
+document.addEventListener("click", (e) => {
+    if (!e.target.matches(".navMenu a")) return false;
+
+    svg_hamburguer.classList.remove('is-active');
+    nav_Menu.classList.remove('is-active');
+    shape.classList.remove('is-active');
+});
